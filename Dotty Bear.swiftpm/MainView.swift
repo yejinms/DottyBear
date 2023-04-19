9e6a6f51eed7e5b77798ebf712a78841cd58174b
Niko <yejinms@naver.com>
2023-04-20 08:11:05 +0900

    MainView 에서 MapView 로 이동하는 네비 버튼 완성

--- Dotty Bear.swiftpm/MainView.swift
+++ Dotty Bear.swiftpm/MainView.swift
@@ -1,77 +1,68 @@
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     
     @State private var showButton = false
+    @State var tag:Int? = nil
     
     var body: some View {
-        ZStack{
-            
-            Image("background_green")
-            
-            VStack {
-                Text("Dotty Bear")
-                    .fontWeight(.bold)
-                    .font(Font.custom("Hoefler Text Black", size: 68))
-                    .foregroundColor(Color(hex: 0x5d097e))
-                    .padding(.top, 60)
-                    .padding(.bottom, -8)
-                
-                
-                Image("dotty1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .offset(x: 35, y:10)
-                    .scaledToFit()
-                    .frame(width: 490)
-                    .ignoresSafeArea()
-                
+        
+        NavigationView{
+            ZStack{
+                NavigationLink(destination: EmptyView(), tag: 1, selection: self.$tag ) {
+                    EmptyView()
+                }
                 ZStack{
                     
-                    NavigationView {
-                        
-                        List {
-                            ImageSlider()
-                                .frame(height: 270)
-                        }
-                    }
-                    .frame(height: 330)
+                    Image("background_green")
                     
-                    if showButton {
-                        
-                        Button (action:{
-                            print(true)
-                        }) {
-                            
-                            HStack{
-                                Image(systemName: "arrow.right.circle")
-                                    .resizable()
-                                    .frame(width: 60, height: 60)
-                            }}
-                        .tint(Color(hex:  0xd2ebab))
-                        .shadow(radius: 3)
-                        .offset(x:140, y:110)
-                        
-                        
-                        
-                        
-                    }
                     
-                    //                    Rectangle()
-                    //                        .frame(height: 370.0)
-                    //                        .foregroundColor(Color(hex: 0xfffdf8))
-                    //                        .padding(.top, -10)
-                    //
-                    
-                }.padding(.top, -15)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
-                            showButton = true
-                            
-                            
-                        }
+                    VStack {
+                        Text("Dotty Bear")
+                            .fontWeight(.bold)
+                            .font(Font.custom("Hoefler Text Black", size: 68))
+                            .foregroundColor(Color(hex: 0x5d097e))
+                            .padding(.top, 60)
+                            .padding(.bottom, -8)
                         
+                        Image("dotty1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .offset(x: 35, y:10)
+                            .scaledToFit()
+                            .frame(width: 490)
+                            .ignoresSafeArea()
+                        
+                        ZStack{
+                            NavigationView {
+                                List {
+                                    ImageSlider()
+                                        .frame(height: 270)
+                                }
+                            }
+                            .frame(height: 330)
+                            //                            .foregroundColor(Color(hex: 0xfffdf8)) // 아이보리
+                            
+                        }.padding(.top, -15)
                     }
+                }
+                
+                if showButton {
+                    Button (action:{
+                        self.tag = 1
+                    }) {    Image(systemName: "arrow.right.circle")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                    }
+                    .tint(Color(hex:  0xd2ebab))
+                    .shadow(radius: 3)
+                    .offset(x:140, y:350)
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+                    showButton = true
+                }
             }
         }
     }
@@ -84,7 +75,7 @@ struct ContentView: View {
 
 struct ContentView_Preview: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
 
--- Dotty Bear.swiftpm/MapView.swift
+++ Dotty Bear.swiftpm/MapView.swift
@@ -5,4 +5,24 @@
 //  Created by Niko Yejin Kim on 2023/04/20.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct MapView: View {
+    
+    
+    var body: some View {
+        Text("TT")
+    }
+    
+    
+}
+
+
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        MapView()
+    }
+}
